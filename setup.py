3bcd69f88abdd2be1a1e1db7e109260a77ea8737
dfoulks <dfoulks@apache.org>
2020-01-31 13:18:06 -0600

    Got everything set up as its own module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pynspawn',
+      version='0.1',
+      description='Python wrapper for debootstrap',
+      url='http://github.com/dfoulks1/pynspawn',
+      author='Drew Foulks',
+      author_email='dfoulks@gmail.com',
+      license='Apache v2 License',
+      packages=['pynspawn'],
+      zip_safe=False)
